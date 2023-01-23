1d767d5befb2febaa5d09f5b4a57d59ee05f2cdc
TanasitISTG <tanasit.istg@mail.kmutt.ac.th>
2023-01-23 20:30:11 +0700

    first

---
+++ SearchWordInDocx/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using Spire.Doc;
+
+namespace SearchWordInDocx
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            try {
+                Document document = new Document();
+                string path;
+                
+                if (args.Length > 0)
+                {
+                    path = args[0];
+                }
+                else
+                {
+                    Console.Write("Enter the path of folder that contains the document: ");
+                    path = Console.ReadLine();
+                }
+                path = path.Replace("\"", "");
+                
+                Console.Write("Enter the word to search: ");
+                string word = Console.ReadLine();
+                SaveDocumentToStream(document, path, word);
+                
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+
+        private static void SaveDocumentToStream(Document document, string path, string word)
+        {
+            MemoryStream finalStream = new MemoryStream();
+            string[] files = Directory.GetFiles(path);
+            foreach (string file in files)
+            {
+                document.LoadFromFile(file);
+                
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    document.SaveToStream(stream, FileFormat.Txt);
+                    stream.Position = 0;
+                    stream.CopyTo(finalStream);
+                }
+            }
+
+            finalStream.Position = 0;
+            SearchWord(finalStream, word, files, document);
+        }
+
+        private static void SearchWord(MemoryStream stream, string word, string[] files, Document document)
+        {
+            stream.Position = 0;
+            bool found = false;
+            foreach (string file in files)
+            {
+                document.LoadFromFile(file);
+                using (MemoryStream docStream = new MemoryStream())
+                {
+                    document.SaveToStream(docStream, FileFormat.Txt);
+                    docStream.Position = 0;
+                    StreamReader reader = new StreamReader(docStream);
+                    string text = reader.ReadToEnd();
+                    
+                    if (text.ToLower().Contains(word.ToLower()))
+                    {
+                        Console.WriteLine("\"" + word + "\"" + " found in document " + Path.GetFileName(file));
+                        found = true;
+                    }
+                }
+            }
+            
+            if (!found)
+            {
+                Console.WriteLine("The word is not found in any document.");
+            }
+        }
+    }
+}
---
+++ SearchWordInDocx/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SearchWordInDocx")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SearchWordInDocx")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5050AE49-2A33-4F16-BC7B-A30D60B3A7D1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
